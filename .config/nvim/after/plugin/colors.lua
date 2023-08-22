1e8a8b558136881f200a90000c20af502ff9944a
Pedro Folloni Pesserl <fpesserl7@gmail.com>
2023-08-21 21:57:48 -0300

    add way to switch between transparent and colored background neovim

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,5 +1,15 @@
 vim.cmd.colorscheme("pinkmare")
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+function transparent_bg()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+end
+
+function color_bg()
+    vim.api.nvim_set_hl(0, "Normal", { bg = "#202330" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#202330" })
+    vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "#202330" })
+end
+
+transparent_bg()
