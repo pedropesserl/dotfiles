0dfab34a14521cb63cf3895d4b68a29622b1af8b
Pedro Folloni Pesserl <pfp22@inf.ufpr.br>
2025-05-07 11:27:30 -0300

    configurar folding nvim

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -15,3 +15,10 @@ vim.g.loaded_matchparen = false
 vim.opt.path = vim.opt.path + "/usr/include/c++/*"
 vim.opt.path = vim.opt.path + "/usr/include/x86_64-linux-gnu/"
 vim.opt.path = vim.opt.path + "/usr/lib/gcc/x86-64-linux-gnu/9/include"
+
+vim.opt.hlsearch = false
+vim.opt.foldmethod = "indent"
+-- vim.cmd([[
+--     au BufRead * normal zR
+-- ]])
+vim.api.nvim_create_autocmd({'BufEnter'}, {command = "normal zR"})
